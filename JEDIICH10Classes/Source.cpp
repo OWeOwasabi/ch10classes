b2b5002725336e890b638d8bea7bc7692d40a514
oweowasabi <oweowasabi@STA-LAB-SD-10>
2018-11-02 13:44:11 -0500

    Finish project

--- JEDIICH10Classes/Source.cpp
+++ JEDIICH10Classes/Source.cpp
@@ -25,6 +25,21 @@ int main() {
 	cout << "Thank you. This is how much time has been elapsed in the day in seconds: " << userClock.elapsed_time() << endl; 
 	// demonstrate the remaining_time() function
 	cout << "This is how much time there is left in the day: " << userClock.remaining_time() << endl;
+	// demonstrate the military_to_12() function
+	userClock.military_to_12();
+	cout << "This is the time in 12 hr format: ";
+	userClock.print_time();
+	// demonstrate the twelve_to_military function
+	cout << "This is the time back into military format: ";
+	userClock.twelve_to_military();
+	userClock.print_time();
+
+	system("pause");
+
+	// declare our own clock for the final demonstration
+	cout << endl << endl << "For the final demonstration, we set our own clock to 23:46:02. We can display the time difference between your clock and ours." << endl;
+	clockType testClock(23, 46, 2);
+	testClock.compare_time(userClock);
 
 
 	system("pause");
